0c760f41c6b8d6d8b8cf93ba834912d5ac11037b
alexander.schmidt <alexander.schmidt@asuras.de>
2021-12-15 23:57:17 +0100

    aoc2021 d15 p2

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlinx.multik.api.d2array
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.ndarray
+import org.jetbrains.kotlinx.multik.api.zeros
 import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
 import org.jetbrains.kotlinx.multik.ndarray.data.get
 import org.jetbrains.kotlinx.multik.ndarray.data.set
@@ -11,7 +12,6 @@ import org.jetbrains.kotlinx.multik.ndarray.operations.*
 // graph bauen mit matrix einmal alle x durch, einmal alle y durch
 
 data class Node(val x: Int, val y: Int, val weight: Int, var visited: Boolean = false)
-data class Edge(val x1: Int, val y1: Int, val x2: Int, val y2: Int)
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -22,38 +22,51 @@ fun main() {
             val (x, y) = index
             nodes.add(Node(x, y, i))
         }
-        val adj = mk.d2array(size, size) { Int.MAX_VALUE }
-        nodes.first { it.x == 0 && it.y == 0 }.visited = true
-        adj[0, 0] = 0
-        while (nodes.any { !it.visited }) {
-            val visitedNodes = nodes.filter { it.visited }
-            val unvisitedNodes = nodes.filterNot { it.visited }
-            // vermutlich ist da noch ein (nicht so schlimmer) fehler drinne
-            visitedNodes.forEach { vn ->
-                val neighbors = getNeighbors(vn, nodes);
-                neighbors.forEach {
-                    it.visited = true
-                    if (it.weight + adj[vn.x, vn.y] < adj[it.x, it.y]) adj[it.x, it.y] = it.weight + adj[vn.x, vn.y]
-                }
-            }
-        }
-        return adj[size-1, size-1]
+        return dijkstra(nodes, size)
     }
 
+    // super slow
     fun part2(input: List<String>): Int {
-        // schauen, wie man matrizen expandiert oder flatmapt
-        // danach kann man für das raster ja die matrix nehmen und mit den koordinaten verrechnen
-        // am ende wieder eine große matrix machen und hoffen, das djikstra fix genug ist
-        return 0
+        val baseSize = input.first().length
+        val size = baseSize * 5
+        val inputArray: D2Array<Int> = readMap(input)
+        val map: D2Array<Int> = mk.zeros<Int>(size, size).mapMultiIndexed { index, _ ->
+            val (x, y) = index
+            val newValue = inputArray[x % baseSize, y % baseSize] + x / baseSize + y / baseSize
+            if (newValue > 9) newValue - 9 else newValue
+        }
+        val nodes = mutableListOf<Node>()
+        map.forEachMultiIndexed { index, i ->
+            val (x, y) = index
+            nodes.add(Node(x, y, i))
+        }
+        return dijkstra(nodes, size)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")
     check(part1(testInput) == 40)
+    check(part2(testInput) == 315)
 
     val input = readInput("Day15")
     println(part1(input))
     println(part2(input))
+}
+
+fun dijkstra(nodes: List<Node>, size: Int): Int {
+    val adj = mk.d2array(size, size) { Int.MAX_VALUE }
+    adj[0, 0] = 0
+    while (nodes.any { !it.visited }) {
+        val unvisitedNodes = nodes.filterNot { it.visited }
+        val vn = unvisitedNodes.minByOrNull { adj[it.x, it.y] }!!
+        vn.visited = true
+        val neighbors = getNeighbors(vn, unvisitedNodes)
+        neighbors.forEach {
+            if (it.weight + adj[vn.x, vn.y] < adj[it.x, it.y]) adj[it.x, it.y] = it.weight + adj[vn.x, vn.y]
+        }
+    }
+    return adj[size - 1, size - 1]
+
 }
 
 private fun getNeighbors(node: Node, nodes: List<Node>): List<Node> {
